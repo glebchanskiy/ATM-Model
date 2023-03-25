3d16e26702a109271fa4c35119041ce910505584
Levkov Gleb <Glebchanskiy@yandex.by>
2023-03-25 11:36:37 +0300

    fix: fixed imports

--- lab4/server/migrations/env.py
+++ lab4/server/migrations/env.py
@@ -5,8 +5,8 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
-from models import metadata
+from config.db_config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+from db.tables import metadata
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
